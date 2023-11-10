4d3055ecaa0c4fce80905259ecbdcd18b745a967
iter <iter@soa.ac.in>
2023-11-10 14:30:00 +0530

    new file:   AD/lab6/heap_sort.java 	modified:   AD/lab6/q4.java

--- AD/lab6/q4.java
+++ AD/lab6/q4.java
@@ -5,25 +5,36 @@ class q4
   
   static int maxAbsDiff(int arr[], int n)
   {
-
     Arrays.sort(arr);
+    System.out.println("min dif arr pair "+arr[n-1]+" "+arr[0]);
     return arr[n-1] - arr[0];
+    
   }
+ 
   static int findMinDiff(int[] arr, int n)
     {
+      int max_m = 0;
+      int max_min = 0;
         int diff = Integer.MAX_VALUE;
         for (int i = 0; i < n - 1; i++)
             for (int j = i + 1; j < n; j++)
-                if (Math.abs((arr[i] - arr[j])) < diff)
+                if (Math.abs((arr[i] - arr[j])) < diff){
                     diff = Math.abs((arr[i] - arr[j]));
- 
+                    max_m=arr[i];
+                    max_min = arr[j];
+                }
+
+
+        System.out.println("max dif arr pair "+max_m+" "+max_min);
         return diff;
     }
 
   public static void main(String args[]) {
     int arr[] = {2,1,5,3};
     int n = arr.length;
-    System.out.println(maxAbsDiff(arr, n)); 
-    System.out.println(findMinDiff(arr, n)); 
+    int a = maxAbsDiff(arr, n);
+    int b = findMinDiff(arr, n);
+    System.out.println(a); 
+    System.out.println(b); 
   }
 }
